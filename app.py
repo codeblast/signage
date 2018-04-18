ee74344d1ae5eae1e925c8c0bffaf3abba62cdec
Gert Lombard <gert.lombard@gmail.com>
2018-04-18 00:04:07 -0700

    Add a Dockerfile for /admin

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 
 from api.checkin import checkin
 
@@ -9,13 +9,9 @@ app.register_blueprint(checkin)
 @app.route('/')
 def hello():
     # TODO(glombard): figure out how to deal with this 8080 vs 80 port split
-    return ('<a href="/admin/checkin.html">Activate check-in</a> '
+    # so we don't have to hardcode the localhost here...
+    return ('<a href="http://localhost:8081/admin/checkin.html">Activate check-in</a> '
     	    'or <a href="http://localhost/pages/checkin.html">view FIDS page</a>?')
-
-
-@app.route('/admin/<path:path>')
-def send_admin(path):
-    return send_from_directory('admin', path)
 
 
 if __name__ == '__main__':
